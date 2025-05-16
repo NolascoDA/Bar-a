1197ffadd9fb6e546389f4edd95681c3e6c2766d
NolascoDA <160720147+NolascoDA@users.noreply.github.com>
2025-05-16 12:07:05 +0200

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-#j_fz@j8jiy8+ymey3kdhlvl%6(%s9!y%#59#dmd2l1erww05t"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
